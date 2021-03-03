b3b23bf0aa3fc8c751bbfc5397f1d0165900c8e8
Francesco Vidaich <francescovidaich@gmail.com>
2021-03-03 16:27:04 +0100

    Files for objects + tests on A/sigma ratios

---
+++ bandit_tests/MIS_estimators.py
@@ -0,0 +1,198 @@
+
+import numpy as np
+
+
+
+###############################
+### MIS estimator Francesco ###
+###############################
+
+def MIS_estimator(nu, thetas, rewards, t, return_type=0, beta=1):
+    
+    # Check if rewards and thetas have the same lenght
+    assert len(thetas) == len(rewards), "Thetas and rewards have different lengths"
+#     if len(thetas) != len(rewards):
+#         print("\n ----- Thetas and rewards have different lengths ----- \n")
+#         return None
+
+    # Compute time of past rewards and prepare array for them
+    t0 = (t+1) - len(rewards)
+    timesteps = np.arange(t0, t+1)
+    sum_terms = np.array([]) 
+    
+    # Compute constant normalization term (for beta<1)
+    if beta<1:
+        beta_normalization = (1-beta)/(1-beta**(len(rewards)-1))
+        
+    # Compute the weighted rewards inside the time window (with lenght alpha)
+    for i in timesteps:
+        
+        # Compute importance weight given the type of the estimator (with beta ir not)
+        IS_weight = nu.theta_pdf(thetas[i-t0], t+1) / nu.theta_pdf(thetas[i-t0], i)
+        if beta == 1:
+            IS_weight = IS_weight / len(rewards)
+        else:
+            IS_weight = IS_weight * beta_normalization * beta**(t-i)
+        
+        # IF we are interested in the weights, don't compute weighted rewards
+        if return_type == 2:
+            sum_terms = np.append(sum_terms, IS_weight)
+        else:
+            IS_reward = rewards[i-t0] * IS_weight
+            sum_terms = np.append(sum_terms, IS_reward)
+    
+    # If we are interested in just the estimated performance, sum the weighted rewards
+    if return_type == 0:
+        return np.sum(sum_terms)
+    else:
+        return sum_terms
+    
+    
+    
+    
+    
+############################
+### MIS estimator Pierre ###
+############################
+    
+def MIS_estimator_other(nu, thetas, rewards, alpha=10, beta=1):
+    
+    # Check if rewards and thetas have the same lenght
+    assert len(thetas) == len(rewards), "Thetas and rewards have different lengths"
+    assert len(thetas) > alpha, "Not enough data for the estimator"
+
+    # Compute time of past rewards and prepare array for them
+    t = len(thetas)
+    timesteps = np.arange(t-alpha, t)
+    IS_est = []
+    
+    # Compute constant normalization term (for beta<1)
+    if beta<1:
+        beta_normalization = np.array([beta**(t-i) for i in timesteps])
+        beta_normalization *= (1-beta)/(1-beta**(alpha-1))
+    else: 
+        beta_normalization = 1/alpha
+        
+        
+    # Compute the weighted rewards inside the time window (with lenght alpha)
+    IS_weights = np.array([nu.theta_pdf(thetas[i], t) / nu.theta_pdf(thetas[i], i) for i in timesteps])
+    IS_est  = beta_normalization * IS_weights * rewards[timesteps]
+
+    return IS_est, np.sum(IS_est), IS_weights#, rewards[timesteps], beta_normalization
+
+
+
+
+
+#############################
+### MIS estimator with BH ###
+#############################
+
+def MIS_bh_estimation(nu, thetas, rewards, alpha=10, beta=1):
+
+    # Check if rewards and thetas have the same lenght
+    assert len(thetas) == len(rewards), "Thetas and rewards have different lengths"
+    assert len(thetas) > alpha, "Not enough data for the estimator"
+
+    # Compute time of past rewards and prepare array for them
+    t = len(thetas)
+    timesteps = np.arange(t-alpha, t)
+    IS_est = []
+
+    # Compute constant normalization term (for beta<1)
+    if beta<1:
+        beta_normalization = np.array([beta**(t-i) for i in timesteps])
+        beta_normalization *= (1-beta)/(1-beta**(alpha-1))
+    else: 
+        beta_normalization = 1/alpha
+
+
+    # Compute the weighted rewards inside the time window (with length alpha)
+    MIS_beta = np.array([np.sum([nu.theta_pdf(thetas[i], k) for k in timesteps]) for i in timesteps ])
+    IS_weights = np.array([nu.theta_pdf(thetas[i], t) for i in timesteps])
+    IS_weights = IS_weights*MIS_beta
+    IS_est  = beta_normalization * IS_weights * rewards[timesteps]
+
+    return IS_est, np.sum(IS_est), IS_weights#, rewards[timesteps], beta_normalization
+
+
+
+
+def range_IS_weight(alpha, range_len):
+    ar = np.concatenate((np.ones(2*alpha+1), np.zeros(range_len+alpha)))
+    roll = np.stack([np.roll(ar, shift=i) for i in range(alpha+range_len+1)])
+    return roll[:,alpha:-alpha-1]
+    
+
+    
+    
+def MIS_bh_estimation_range(nu, thetas, rewards, range_len, alpha=10, beta=1):
+    t = len(thetas)
+    shift = t-range_len-alpha-1
+    timesteps = np.arange(t-alpha-range_len, t).reshape(-1,1)
+    range_weight = range_IS_weight(alpha, range_len)
+    proba_nu = np.zeros_like(range_weight)
+    
+    for (i,k) in tqdm(zip(np.where(range_weight)[0],np.where(range_weight)[1])):
+        proba_nu[i,k] = nu.theta_pdf(thetas[i], k) 
+        
+    if beta<1:
+        beta_normalization = np.array([beta**(t-i) for i in timesteps])
+        beta_normalization *= (1-beta)/(1-beta**(alpha-1))
+    else: 
+        beta_normalization = 1/alpha
+        
+    IS_est_all = []
+    timesteps = np.arange(shift,shift+alpha)
+        
+    for t in range(range_len):
+        MIS_beta = np.array([ np.sum(proba_nu[t:t+alpha,i]) for i in range(t,t+alpha)])
+        IS_weights = proba_nu[t+alpha,t:t+alpha]
+        IS_weights = IS_weights*MIS_beta
+        IS_est = beta_normalization * IS_weights * rewards[timesteps+t]
+        IS_est_all.append(np.sum(IS_est))
+        
+    return IS_est_all
+    
+
+    
+
+########################################
+### MIS estimator with BH - Variance ###
+########################################
+    
+    
+def renyi_normal(alpha, mu_1, sigma_1, mu_2, sigma_2):
+    """ D_\alpha(P_1||P_2)"""
+    mu_diff = (mu_1-mu_2).reshape(-1,1)
+    sigma_alpha = alpha*sigma_1 + (1-alpha)*sigma_2
+    renyi_div = np.linalg.det(sigma_alpha)
+    renyi_div /= (np.linalg.det(sigma_1)**(1-alpha)*np.linalg.det(sigma_2)**alpha)
+    renyi_div = -np.log(renyi_div)/(2*(alpha-1))
+    renyi_div += alpha/2*np.matmul(mu_diff.T, np.matmul(np.linalg.invsigma_alpha), mu_diff)
+    return renyi_div
+
+def exp_2_renyi_div_normal_1d(mu_1, sigma_1, mu_2, sigma_2, alpha):
+    sigma_alpha = alpha*sigma_1 + (1-alpha)*sigma_2
+    renyi_div = np.exp((mu_1-mu_2)**2/sigma_alpha)
+    renyi_div /= np.sqrt(sigma_alpha*sigma_1/sigma_2)
+    return renyi_div
+    
+def MIS_bh_variance(nu, thetas, rewards, R_inf=1, alpha=10, beta=1):
+    t = len(thetas)
+    timesteps = np.arange(t-alpha, t)
+
+    renyi_div = [exp_2_renyi_div_normal_1d(nu.theta_mean(t), nu.sigma_theta, nu.theta_mean(i), nu.sigma_theta, alpha) for i in timesteps]
+    renyi_div = np.array(renyi_div)
+    
+    return R_inf*alpha/np.sum(1/renyi_div)
+    
+# def MIS_bh_variance_range(nu, thetas, rewards, range_len, R_inf=1, alpha=10, beta=1):
+#     t = len(thetas)
+#     shift = t-range_len-alpha-1
+#     timesteps = np.arange(t-alpha-range_len, t).reshape(-1,1)
+#     range_weight = range_IS_weight(alpha, range_len)
+#     proba_nu = np.zeros_like(range_weight)
+    
+#     for (i,k) in tqdm(zip(np.where(range_weight)[0],np.where(range_weight)[1])):
+#         proba_nu[i,k] = nu.theta_pdf(thetas[i], k) 
---
+++ bandit_tests/bandit_classes.py
@@ -0,0 +1,274 @@
+
+import numpy as np
+import scipy.stats
+
+# Define sigmoid function
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+
+#######################
+###   ENVIRONMENT   ###
+#######################
+
+class environment:
+    
+    ### Define the environment: NS_context=True  -> non-stat context distribution    
+    ###                         NS_context=False -> non-stat reward function
+    ###                         remove_contexts -> x_t=0 always, reward function becomes non-stat
+    def __init__(self, NS_context, remove_context=False, sigma_x=1, t0=0, **kwargs):
+        
+        # Store the given parameters
+        self.t = t0
+        self.t_init = t0
+        self.sigma_x = sigma_x
+        self.remove_context = remove_context
+        
+        # If we don't want to use context, use a non-stat reward function
+        if remove_context:
+            self.NS_context = False
+        else:
+            self.NS_context = NS_context
+        
+        # if params that define the mean are not given, take random ones
+        self.var_names = ['A', 'phi', 'psi'] 
+        for var in self.var_names:
+            if var in kwargs.keys():
+                setattr(self, var, kwargs[var])
+            else:
+                setattr(self, var, np.random.rand())
+                
+    def reset(self):
+        self.t = self.t_init
+    
+
+    
+    ### Definition of the non-stationary process: 
+    ###    If NS_context = True,  it defines the mean of the context distribution
+    ###    If NS_context = False, it defines a term in the reward function
+    def non_stat_process(self, t):
+        return self.A * np.sin(self.phi*t + self.psi)
+        
+    
+    
+    ### Compute the context mean at present timestep 'self.t'. If context distr is 
+    ### stationary or if x_t is fixed to 0, the mean is always equal to 0
+    def x_mean(self, t=None):
+        if self.remove_context or not(self.NS_context):
+            return 0
+        else:
+            return self.non_stat_process(self.t)
+
+        
+    ### Sample a context from the distibution at time 'self.t'
+    def sample_x(self):
+        if self.remove_context:
+            return 0
+        else:
+            return scipy.stats.norm.rvs(loc   = self.x_mean(), 
+                                        scale = self.sigma_x)
+    
+    
+    
+    
+    ### Sample reward from a Bernoulli distribution that depends on the action and the context; 
+    ### The parameter is computed differently if we have non-stat contexts or non-stat rewards
+    def get_reward(self, x, action, get_p=False):
+        
+        # Compute bernoulli parameter for the correct case
+        if self.NS_context:
+            p_t = sigmoid( x - self.x_mean() )
+        else:
+            #initial_reward = self.non_stat_process(self.t)                  #      ----  ATTENTION  ----
+            #p_t = sigmoid( x + initial_reward )
+            p_t = self.non_stat_process(self.t) + 0.5
+        
+        # If we want to study the distributions, return parameter instead of reward
+        if get_p:
+            if action:
+                return p_t
+            else:
+                return 1-p_t
+        
+        # Sample a reward given the chosen action
+        if action:
+            return scipy.stats.bernoulli.rvs(p_t)
+        else:
+            return scipy.stats.bernoulli.rvs(1 - p_t)
+            
+            
+    
+    
+    ### Use the hyperpolicy nu to perform many subsequent steps
+    ### Returns the evolution of many variables
+    def play(self, nu, n_steps, get_p=False):
+        
+        contexts = np.array([])
+        thetas   = np.array([])
+        rewards  = np.array([])
+        actions  = np.array([])
+        non_stat_process = np.array([])
+        theta_means      = np.array([])
+    
+        # At each timestep...
+        for i in range(n_steps):
+            
+            # Sample a policy from the hyperpolicy 
+            # (given t and the tipe of non-stationarity)
+            policy = nu.sample_policy(self.t, self.NS_context)
+            
+            # Sample a context from the corresponding distribution
+            x_t = self.sample_x()
+            
+            # Perform a step sampling the action from current policy 
+            action = policy.action(x_t)
+            reward = self.get_reward(x_t, action, get_p)
+            
+            # Store current values in the arrays
+            contexts = np.append(contexts, x_t)
+            thetas   = np.append(thetas, policy.theta)
+            rewards  = np.append(rewards, reward)
+            actions  = np.append(actions, action)
+            non_stat_process = np.append(non_stat_process, self.non_stat_process(self.t))
+            theta_means   = np.append(theta_means, nu.theta_mean(self.t))
+            
+            # Update time variable
+            self.t += 1
+
+        return contexts, thetas, rewards, actions, non_stat_process, theta_means
+    
+    def play_deter(self, nu, n_steps, get_p=False):
+        
+        contexts = np.array([])
+        thetas   = np.array([])
+        rewards  = np.array([])
+        actions  = np.array([])
+        non_stat_process = np.array([])
+        theta_means      = np.array([])
+    
+        # At each timestep...
+        for i in range(n_steps):
+            
+            # Sample a policy from the hyperpolicy 
+            # (given t and the tipe of non-stationarity)
+            policy = policy_class(nu.theta_mean(self.t), self.NS_context, False)
+            
+            # Sample a context from the corresponding distribution
+            x_t = self.sample_x()
+            
+            # Perform a step sampling the action from current policy 
+            action = policy.action(x_t)
+            reward = self.get_reward(x_t, action, get_p)
+            
+            # Store current values in the arrays
+            contexts = np.append(contexts, x_t)
+            thetas   = np.append(thetas, policy.theta)
+            rewards  = np.append(rewards, reward)
+            actions  = np.append(actions, action)
+            non_stat_process = np.append(non_stat_process, self.non_stat_process(self.t))
+            theta_means   = np.append(theta_means, nu.theta_mean(self.t))
+            
+            # Update time variable
+            self.t += 1
+
+        return contexts, thetas, rewards, actions, non_stat_process, theta_means
+    
+    
+    
+    
+########################
+###   POLICY CLASS   ###
+########################
+    
+class policy_class:
+    
+    ### Theta is the only param and reprsents an estimate of the non-stat 
+    ### process of the environment (that represent different )
+    def __init__(self, theta, NS_context, stochastic=True):
+        self.theta = theta
+        self.stochastic = stochastic
+        self.NS_context = NS_context
+    
+    ### Return the bernoulli distr over the actions given the context
+    ### (makes sense only if policy is stochastic, i.e. action-based case)
+    def bernoulli_distr(self, x):
+        if self.NS_context:
+            p = sigmoid(x - self.theta)
+        else:
+            p = sigmoid(x + self.theta)
+        return (1-p,p)
+        
+     
+    ### Choose action given the sampled context (and depending on the problem)
+    def action(self, x):
+        
+        ### IF env has nonstationary Context distribution,
+        ### action = 1 has higher prob (or chosen) if x-theta > 0
+        if self.NS_context:
+            if self.stochastic:
+                p = sigmoid(x - self.theta)
+                return scipy.stats.bernoulli.rvs(p)
+            else:
+                return (x - self.theta > 0)
+            
+        ### IF env has nonstationary reward function,
+        ### action = 1 has higher prob (or chosen) if x+theta > 0
+        else:
+            if self.stochastic:
+                p = sigmoid(x + self.theta)
+                return scipy.stats.bernoulli.rvs(p)
+            else:
+                return (x + self.theta > 0)
+            
+            
+      
+    
+
+    
+    
+#######################
+###   HYPER-POLICY  ###
+#######################
+    
+class hyperpolicy:
+        
+    def __init__(self, param_based=True, sigma_theta=1, **kwargs):
+        
+        # Store the given parameters
+        self.sigma_theta = sigma_theta
+        self.stochastic = param_based
+        
+        # if params that define the mean are not given, take random ones        
+        self.var_names = ['A', 'phi', 'psi'] 
+        for var in self.var_names:
+            if var in kwargs.keys():
+                setattr(self, var, kwargs[var])
+            else:
+                setattr(self, var, np.random.rand())
+               
+            
+    
+    def theta_mean(self, t):
+        return self.A * np.sin(self.phi*t + self.psi)
+    
+        
+    def theta_pdf(self, theta, t):
+        return scipy.stats.norm.pdf(theta, loc=self.theta_mean(t), scale=self.sigma_theta)
+    
+    
+    def sample_theta(self, t):
+        if self.stochastic:
+            return scipy.stats.norm.rvs(loc=self.theta_mean(t), scale=self.sigma_theta)
+        else:
+            return self.theta_mean(t)
+
+    def sample_policy(self, t, NS_context):
+        theta = self.sample_theta(t)
+        return policy_class(theta, NS_context, not(self.stochastic) )
+    
+    
+    #def update_params(self, delta_params):
+        
+        # TO DO
+        
